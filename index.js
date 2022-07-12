7ba3cacfbe6be7bed521d14b16721140d1169186
Vu-Phi-Ung-Vuong <104881683+Vu-Phi-Ung-Vuong@users.noreply.github.com>
2022-07-12 11:56:17 +0700

    test1

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const cors = require('cors')
 const rfs = require('rotating-file-stream')
 const path = require('path')
 
-const route = require('./src/routes')
-const db = require('./models/index')
 const passport = require('passport')
 const compression = require('compression')
 
