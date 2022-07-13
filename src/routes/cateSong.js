75d286e672a055d8a6c57162a6203012c91fd775
TanCuong13122002 <yukicuong66@gmail.com>
2022-07-13 10:20:53 +0700

    AddCateSong
    Add CRUD of SongCategoryModel

---
+++ src/routes/cateSong.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const CategoriesSongController = require('../app/controllers/CategoriesSongController')
+const passport = require('passport')
+
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    CategoriesSongController.index
+)
+
+router.post('/create', 
+passport.authenticate('jwt', { session: false }),
+CategoriesSongController.create)
+
+router.put(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    CategoriesSongController.update
+)
+
+
+router.delete(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    CategoriesSongController.delete
+)
+
+module.exports = router
