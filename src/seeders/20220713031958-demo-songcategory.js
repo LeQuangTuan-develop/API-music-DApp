0b9a8442919e82fd0e4313f0f4f0da8a9e687cac
WolfHybrid <lequangtuan1605@gmail.com>
2022-07-13 16:22:53 +0700

    fix: seeder

--- src/seeders/20220713031958-demo-songcategory.js
+++ src/seeders/20220713031958-demo-songcategory.js
@@ -1,28 +1,26 @@
-'use strict';
+'use strict'
 
-const  { faker } = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker')
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    var Songcategory =[] 
-    for (var i = 0;i < 100;i++) {
-      Songcategory.push({
-        name: faker.music.genre(),
-        imageUrl:faker.image.imageUrl(),
-      })
-      
-    }
-    await queryInterface.bulkInsert('songcategories', Songcategory)
-  },
+    async up(queryInterface, Sequelize) {
+        var Songcategory = []
+        for (var i = 0; i < 100; i++) {
+            Songcategory.push({
+                name: faker.music.genre(),
+                imageUrl: faker.image.imageUrl(),
+            })
+        }
+        await queryInterface.bulkInsert('songcategories', Songcategory)
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-     await queryInterface.bulkDelete('songcategories', null, {})
-  }
-};
-
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+        await queryInterface.bulkDelete('songcategories', null, {})
+    },
+}
