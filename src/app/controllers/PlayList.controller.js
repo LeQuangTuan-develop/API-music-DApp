26620f1ea6d4b65fd5a40a016554b96417366936
Vu-Phi-Ung-Vuong <104881683+Vu-Phi-Ung-Vuong@users.noreply.github.com>
2022-07-19 11:35:22 +0700

    19/7

--- src/app/controllers/PlayList.controller.js
+++ src/app/controllers/PlayList.controller.js
@@ -1,4 +1,14 @@
 const httpStatus = require('http-status')
-const CategoriesService = require('../services/List.service')
 const { dataResponse } = require('../../utils/response')
+const PlayListService = require('../services/PlayList.service')
 
+class PlayListController{
+    async show(req, res, next) {
+        try {
+            const cates = await PlayListService.getListNewSong()
+            res.status(httpStatus.OK).json(dataResponse(httpStatus.OK, cates))
+        } catch (error) {
+            next(error)
+        }
+    }
+}
