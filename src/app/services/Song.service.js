f9007ad03ff33b2d5cd077cf700cc18244b857a9
hoangduy281 <hoangduy2812000@gmail.com>
2022-07-31 23:45:59 +0700

    update-PENDING-HIDE-REFUSE-PUBLISH
    update-PENDING-HIDE-REFUSE-PUBLISH

--- src/app/services/Song.service.js
+++ src/app/services/Song.service.js
@@ -1,6 +1,8 @@
 const { Song } = require('../models')
 const elasticClient = require('../../configs/elastic-client.config')
-
+const ApiError = require('../../utils/apiError')
+const { songStatus } = require('../../configs/songStatus.config')
+const httpStatus = require('http-status')
 class SongService {
     async createSong(data) {
         data.lyricRendered = JSON.stringify(data.lyricRendered)
@@ -39,9 +41,81 @@ class SongService {
         if (!song) {
             throw new ApiError(
                 httpStatus.BAD_REQUEST,
-                'This song does not exist',
+                'This song does not exist'
             )
         }
+        return song
+    }
+
+    async updatePending(id) {
+        const songId = await Song.findByPk(id)
+        if (!songId)
+            throw new ApiError(
+                httpStatus.BAD_REQUEST,
+                'This song does not exist'
+            )
+        const song = await Song.update(
+            {
+                status: songStatus.PENDING,
+            },
+            {
+                where: { _id: id },
+            }
+        )
+        return song
+    }
+
+    async updatePublish(id) {
+        const songId = await Song.findByPk(id)
+        if (!songId)
+            throw new ApiError(
+                httpStatus.BAD_REQUEST,
+                'This song does not exist'
+            )
+        const song = await Song.update(
+            {
+                status: songStatus.PUBLISH,
+            },
+            {
+                where: { _id: id },
+            }
+        )
+        return song
+    }
+
+    async updateRefuse(id) {
+        const songId = await Song.findByPk(id)
+        if (!songId)
+            throw new ApiError(
+                httpStatus.BAD_REQUEST,
+                'This song does not exist'
+            )
+        const song = await Song.update(
+            {
+                status: songStatus.REFUSE,
+            },
+            {
+                where: { _id: id },
+            }
+        )
+        return song
+    }
+
+    async updateHide(id) {
+        const songId = await Song.findByPk(id)
+        if (!songId)
+            throw new ApiError(
+                httpStatus.BAD_REQUEST,
+                'This song does not exist'
+            )
+        const song = await Song.update(
+            {
+                status: songStatus.HIDE,
+            },
+            {
+                where: { _id: id },
+            }
+        )
         return song
     }
 }
